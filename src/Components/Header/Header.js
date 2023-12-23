632e2ed85848251c4bf16abf9e95c5b9fbbb4f3d
William David Camacho Sanchez <will@MacBook-Air-de-William.local>
2023-12-22 20:07:07 -0500

    Crear Componente Header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from '../../assets/Logo.jpg'
+import  '../../Components/Header/Header.css'
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="Logo de la Empresa" />
+      <h1>Tienda de Computadoras</h1>
+      <nav>
+        <ul>
+          <li><a href="#inicio">Inicio</a></li>
+          <li><a href="#productos">Productos</a></li>
+          <li><a href="#blog">Blog</a></li>
+          <li><a href="#acerca">Acerca de Nosotros</a></li>
+          <li><a href="#contacto">Contacto</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
